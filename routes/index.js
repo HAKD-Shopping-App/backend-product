396af17299e14b198212c70be314a653ac44a9ff
kasunidilshani <kasunidilshani99@gmail.com>
2022-10-28 09:19:19 +0530

    add view,add,update and delete products

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const actions = require('../methods/actions')
 const router = express.Router()
+const createError = require('http-errors');
 
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+router.post('/product', actions.addProduct)
+router.get('/products', actions.getProducts)
+router.get('/products/:id', actions.getProductById)
+router.put('/product/:id', actions.updateProduct)
+router.delete('/product/:id', actions.deleteProduct)
 router.get('/searchByCategory', actions.searchByCategory)
 
 module.exports = router
